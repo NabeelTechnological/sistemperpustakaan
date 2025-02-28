da3014f6ecd680135a2d1f4f023cbccdadb6f450
Jevicko <Jevickoaradhana@gmail.com>
2025-02-28 10:57:37 +0700

    Delete dsipustaka-main directory

--- dsipustaka-main/content/pustaka/buku/buku_kdbuku_ajax.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$dataKode = $_POST['dataKode'];
-$dataPengarang = $_POST['dataPengarang'];
-$dataJudul = $_POST['dataJudul'];
-
-$dataKodeBuku = kodebuku($dataKode,$dataPengarang,$dataJudul);
-echo json_encode($dataKodeBuku);
-
-?>
--- dsipustaka-main/content/pustaka/buku/buku_tersedia_ajax.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$txtID = $_POST['txtIdBuku'];
-$dataTersedia = isBukuIersedia($koneksidb,$txtID);
-
-echo json_encode($dataTersedia);
-?>
